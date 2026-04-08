e555f791edc9f8ff616ad0f99cb93b0651eb182a
Janis <janis.goldmanis@gmail.com>
2026-04-08 22:01:19 +0300

    quest 2 formatting

--- 2024/quest2.py
+++ 2024/quest2.py
@@ -1,7 +1,7 @@
-with open('input/quest_2_part1.txt', 'r') as file:
+with open("input/quest_2_part1.txt", "r") as file:
     lines = file.readlines()
 
-runic_words = lines[0].strip().split(':')[1].split(',')
+runic_words = lines[0].strip().split(":")[1].split(",")
 inscription = lines[2].strip()
 
 count = sum(inscription.count(word) for word in runic_words)
@@ -9,10 +9,10 @@ count = sum(inscription.count(word) for word in runic_words)
 print(f"Part 1: {count}")
 
 
-with open('input/quest_2_part_2.txt', 'r') as file:
+with open("input/quest_2_part_2.txt", "r") as file:
     lines = file.readlines()
 
-runic_words = lines[0].strip().split(':')[1].split(',')
+runic_words = lines[0].strip().split(":")[1].split(",")
 runic_words = set(runic_words)
 runic_words |= {word[::-1] for word in runic_words}
 inscriptions = [line.strip() for line in lines[2:]]
@@ -20,7 +20,6 @@ inscriptions = [line.strip() for line in lines[2:]]
 runic_symbols = 0
 
 for inscription in inscriptions:
-
     valid_indices = set()
 
     for i in range(len(inscription)):
@@ -35,18 +34,19 @@ print(f"Part 2: {runic_symbols}")
 
 from collections import defaultdict
 
-with open('input/quest_2_part_3.txt', 'r') as file:
+
+with open("input/quest_2_part_3.txt", "r") as file:
     lines = file.readlines()
 
-runic_words = set(lines[0].strip().split(':')[1].split(','))
+runic_words = set(lines[0].strip().split(":")[1].split(","))
 inscriptions = [line.strip() for line in lines[2:]]
 
 ROW_COUNT = len(inscriptions)
 COL_COUNT = len(inscriptions[0])
 
-valid_arr = [['.'] * len(inscriptions[0]) for _ in range(len(inscriptions))]
+valid_arr = [["."] * len(inscriptions[0]) for _ in range(len(inscriptions))]
 
-DIRECTIONS = [(1,0), (-1, 0), (0, 1), (0, -1)]
+DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 words_by_start = defaultdict(list)
 
@@ -84,7 +84,7 @@ for row in range(ROW_COUNT):
                     for i in range(len(word)):
                         r = row + dr * i
                         c = (col + dc * i) % COL_COUNT
-                        valid_arr[r][c] = 'X'
+                        valid_arr[r][c] = "X"
 
-result = sum(row.count('X') for row in valid_arr)
+result = sum(row.count("X") for row in valid_arr)
 print(f"Part 3: {result}")
